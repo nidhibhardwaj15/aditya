26ec49b6fd481d9cde5b1b38b08219610026358c
nidhibhardwaj15 <bhardwajnidhi9877@gamil.com>
2025-02-15 14:00:11 +0630

    first commit

---
+++ Main.java/src/nidhi/ClassA.java
@@ -0,0 +1,13 @@
+package nidhi;
+
+public class ClassA {
+	void meth1() 
+	{
+	  System.out.println("String Arrays convert into int Arrays");
+	  
+	  
+	}
+     public static void main(String[] args) {
+		 new ClassA().meth1();
+	}
+}
